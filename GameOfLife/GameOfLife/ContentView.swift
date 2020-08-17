01f3d105f5e35b2f92fe5d89631468a06ef7e2d6
Robert Keffury <bobby.keffury@me.com>
2020-08-17 10:37:01 -0700

    Added and organized project.

---
+++ GameOfLife/GameOfLife/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GameOfLife
+//
+//  Created by Bobby Keffury on 8/17/20.
+//  Copyright © 2020 Bobby Keffury. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
